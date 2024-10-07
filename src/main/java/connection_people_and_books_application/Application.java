8cbeae27851d8f3faf153bc26c0a188980901ca7
hiblue02 <hiblue02@gmail.com>
2024-10-07 14:00:54 +0900

    init server

---
+++ src/main/java/connection_people_and_books_application/Application.java
@@ -0,0 +1,13 @@
+package connection_people_and_books_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/connection_people_and_books_application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package connection_people_and_books_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
