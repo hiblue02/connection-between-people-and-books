121ffbd3134c1152f106f02cd936e6fd76349d9a
hiblue02 <hiblue02@gmail.com>
2024-11-29 21:06:53 +0900

    feat: BaseEntity를 만든다

---
+++ src/main/java/connection_people_and_books_application/common/model/BaseEntity.java
@@ -0,0 +1,30 @@
+
+package connection_people_and_books_application.common.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
